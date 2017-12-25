42695ea17f67f0018e0a6d43a9d191e8715876be
WMJ improved <6400187+WMJi@users.noreply.github.com>
2017-12-25 16:59:03 +0800

    add comments to adaboost

--- AdaBoost.py
+++ AdaBoost.py
@@ -3,6 +3,8 @@
 
 from numpy import *
 
+
+#数据接口
 def loadSimpleData():
     dataMat = matrix([[1. , 2.1],
         [2. , 1.1],
@@ -12,6 +14,10 @@ def loadSimpleData():
     classLabels = [1.0,1.0,-1.0,-1.0,1.0]
     return dataMat, classLabels
 
+
+#stumpClassify(dataMatrix,dimen,threshVal,threshIneq)
+#单层决策树的分类器，根据输入的值与阀值进行比较得到输出结果，
+#因为是单层决策树，所以只能比较数据一个dimen的值
 def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):
     retArry = ones((shape(dataMatrix)[0],1))
     if threshIneq == 'lt':
@@ -20,6 +26,11 @@ def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):
         retArry[dataMatrix[:,dimen] > threshVal] = -1.0
     return retArry
 
+
+#buildStump(dataArr,classLabels,D)
+#构造单层决策树，这部分的构造的思路和前面的决策树是一样的，只是这里的评价体系不是熵而是加权的错误率，
+#这里的加权是通过数据的权重D来实现的，每一次build权重都会因上一次分类结果不同而不同。
+#返回的单层决策树的相关信息存在字典结构中方便接下来的使用
 #D是权重向量
 def buildStump(dataArr,classLabels,D):
     dataMatrix = mat(dataArr)
@@ -41,7 +52,8 @@ def buildStump(dataArr,classLabels,D):
                 errArr = mat(ones((m,1)))
                 errArr[predictedVals == labelMat] = 0
                 weightedError = D.T * errArr#不同样本的权重是不一样的
-                #print "split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError)
+                #print "split: dim %d, thresh %.2f, thresh ineqal: %s, 
+                #    the weighted error is %.3f" % (i, threshVal, inequal, weightedError)
                 if weightedError < minError:
                     minError = weightedError
                     bestClasEst = predictedVals.copy()
@@ -50,6 +62,10 @@ def buildStump(dataArr,classLabels,D):
                     bestStump['ineq'] = inequal
     return bestStump,minError,bestClasEst
 
+
+#adaBoostTrainDS(dataArr,classLabels,numIt=40)
+#AdaBoost的训练函数，用来将一堆的单层决策树组合起来形成结果。
+#通过不断调整alpha和D来使得错误率不断趋近0，甚至最终达到0。
 def adaBoostTrainDS(dataArr,classLabels,numIt=40):
     weakClassArr = []
     m =shape(dataArr)[0]
@@ -75,6 +91,9 @@ def adaBoostTrainDS(dataArr,classLabels,numIt=40):
             break
     return weakClassArr
 
+
+#adaClassify(datToClass,classifierArr)
+#分类函数，datToClass是要分类的数据，根据生成的一堆单层决策树的分类结果，加权得到最终结果。
 #dataToClass 表示要分类的点或点集
 def adaClassify(datToClass,classifierArr):
     dataMatrix = mat(datToClass)#do stuff similar to last aggClassEst in adaBoostTrainDS
@@ -88,6 +107,8 @@ def adaClassify(datToClass,classifierArr):
         print aggClassEst
     return sign(aggClassEst)
 
+
+#__main__
 def main():
     dataMat,classLabels = loadSimpleData()
     D = mat(ones((5,1))/5)
